4b845151dac43611c0164902cd5702851a0e759b
nico <mr.n.rubert@gmail.com>
2022-02-22 15:22:05 -0500

    Practicing array functions

---
+++ arrays/array.py
@@ -0,0 +1,30 @@
+
+# Create an array
+array = [0,1,2,3,4,5]
+
+# Print every element
+for i in array:
+    print(array[i])
+
+# Access an element in the array
+print(array[2])
+
+# Append element
+array.append(6)
+
+# Insert
+array.insert(2,7)
+
+# Remove
+array.remove(7)
+
+# Pop 
+array.pop()
+
+# Get value
+array.index(6, 0,4)
+
+# Reverse array
+array.reverse()
+
+
